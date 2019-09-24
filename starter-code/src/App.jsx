de5bdcb7d90b304e2d06a6897709581cf8216afe
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-09-24 17:42:08 +0100

    Iteration 2

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import foods from "./foods.json";
+import FoodBox from "./components/foodBox";
+import { isTemplateElement } from "@babel/types";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        name: "José",
+        age: 26,
+        profession: "LT WD IH"
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <button>Add New Food</button>
+        <FoodBox foods={foods} />
+        {/* <div className="container">
+          {foods.map(item => (
+            <div className="row my-4 py-4">
+              
+            </div>
+          ))}
+        </div> */}
       </div>
     );
   }
---
+++ starter-code/src/components/foodBox.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export default class FoodBox extends Component {
+  render() {
+    const foods = this.props.foods;
+    console.log(foods);
+    return (
+      <div>
+        {foods.map(food => (
+          <article className="col-sm-4">
+            {" "}
+            <div className="">
+              {" "}
+              <div className="">
+                {" "}
+                <p>
+                  <strong>{food.name}</strong> <br />
+                  <small>{food.calories}</small>{" "}
+                </p>{" "}
+              </div>{" "}
+            </div>{" "}
+            <div className="">
+              <div className=""></div>{" "}
+            </div>{" "}
+            <div className="">
+              {" "}
+              <figure className="">
+                <img className="w-50" src={food.image} />{" "}
+              </figure>{" "}
+            </div>{" "}
+            <div className="col-sm-4">
+              <input className="" type="number" value="1" />{" "}
+            </div>{" "}
+            <div className="col-sm-4">
+              <button className="">+</button>{" "}
+            </div>{" "}
+          </article>
+        ))}
+      </div>
+    );
+  }
+}
